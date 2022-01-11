40845a04d5be97b5cf64d63b92945271d48b6d7a
Myaddib <Hotabich80@gmail.com>
2022-01-11 15:14:41 +0200

    Error

--- src/geekbrins/ru/GameWindows.java
+++ src/geekbrins/ru/GameWindows.java
@@ -4,28 +4,33 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GameWindows extends JFrame {
-    private static GameWindows game_windows;
 
+    // write your code here
     public static void main(String[] args) {
-	// write your code here
-        game_windows = new GameWindows();
+        GameWindows game_windows = new GameWindows();
         game_windows.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         game_windows.setLocation(200,100);
         game_windows.setSize(906,487);
         game_windows.setResizable(false);
-//        GameField game_Field = new GameField();
-//        game_windows.add(game_Field);
+
+        GameField game_Field = new GameField();
+        game_windows.add(game_Field);
+
         game_windows.setVisible(true);
     }
-//    private static void onRepaint(Graphics g){
-//        g.fillOval(10,10,200,100);
-//    }
-//
-//    private static class GameField extends JPanel{
-//        @Override
-//        protected void paintComponent(Graphics g) {
-//            super.paintComponent(g);
-//            onRepaint(g);
-//        }
-//    }
+
+    private static void onRepaint(Graphics g) { // рисовать
+        g.fillOval(10, 10, 200, 100); //овал
+        g.drawLine(50, 30, 350, 500); // линия раз
+        g.drawLine(45, 40, 275, 431); // линия дваз
+    }
+
+    private static class GameField extends JPanel { //класс понели
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g); //сначало он
+            onRepaint(g);
+        }
+    }
 }
